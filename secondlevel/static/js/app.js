0d87f0287b4cf7a985bfeb2833fddfbebba29c46
Lisa Weinstein <55967990+lisaweinst@users.noreply.github.com>
2020-09-29 11:20:19 -0500

    Update app.js

--- secondlevel/static/js/app.js
+++ secondlevel/static/js/app.js
@@ -1,79 +1,101 @@
-// from data.js
-let tableData = data;
+// Get references to the tbody element, input fields and button
+var $tbody = document.querySelector("tbody");
+var $dateInput = document.querySelector("#datetime");
+var $stateInput = document.querySelector("#state");
+var $cityInput = document.querySelector("#city");
+var $countryInput = document.querySelector("#country");
+var $shapeInput = document.querySelector("#shape");
+var $searchBtn = document.querySelector("#search");
+var $resetBtn = document.querySelector("#reset");
 
-// Creating References
-//pulling out the date from the body tags
-let $tbody = d3.select("tbody");
-let button = d3.select("#filter-btn");
-let pulledDate = d3.select("#datetime");
-let pulledCity = d3.select("#city");
-let pulledState = d3.select("#state");
-let pulledCountry = d3.select("#country");
-let pulledShape = d3.select("#shape");
-let columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+// Add an event listener to the searchButton, call handleSearchButtonClick when clicked
+$searchBtn.addEventListener("click", handleSearchButtonClick);
 
-// Inputing the data into the HTML
-// goes through all of the columns and rows and inputs into a new variable
-let newData = (dataInput) => {
-    dataInput.forEach(filteredSightings => {
-        let row = $tbody.append("tr");
-        columns.forEach(column => row.append("td").text(filteredSightings[column])
-        )
-    });
+// Add an event listener to the resetButton, call handleResetButtonClick when clicked
+$resetBtn.addEventListener("click", handleResetButtonClick);
+
+// Create a copy of the data
+var tableData = data;
+
+// Build table with non-filtered data
+function renderTable() {
+  $tbody.innerHTML = "";
+  for (var i = 0; i < tableData.length; i++) {
+    // Get current address object and fields
+    var address = tableData[i];
+    console.log(address)
+    var fields = Object.keys(address);
+    // Create new row in tbody, set index to be i + startingIndex
+    var $row = $tbody.insertRow(i);
+    for (var j = 0; j < fields.length; j++) {
+      // For each field in address object, create new cell and set inner text to be current value at current address field
+      var field = fields[j];
+      var $cell = $row.insertCell(j);
+      $cell.innerText = address[field];
+    }
+  }
 }
 
-newData(tableData);
+// Build search table for filtered data
+function handleSearchButtonClick() {
+  var filterDate = $dateInput.value;
+  var filterState = $stateInput.value.trim().toLowerCase();
+  var filterCity = $cityInput.value.trim().toLowerCase();
+  var filterCountry = $countryInput.value.trim().toLowerCase();
+  var filterShape = $shapeInput.value.trim().toLowerCase();
 
+  // Filter on date
+  if (filterDate != "") {
+    tableData = data.filter(function (address) {
+      var addressDate = address.datetime;
+      return addressDate === filterDate;
+    });
+  }
+  else { tableData };
 
-// Creating an Event Listener for the Button
-// Setting up the Filter Button for the date
-button.on("click", () => {
+  // Filter on state
+  if (filterState != "") {
+    tableData = tableData.filter(function (address) {
+      var addressState = address.state;
+      return addressState === filterState;
+    });
+  }
+  else { tableData };
 
-    d3.event.preventDefault();
-    
-//removes whitespace
-    let addedDate = pulledDate.property("value").trim();
-    let addedCity = pulledCity.property("value").trim();
-    let addedState = pulledState.property("value").trim();
-    let addedCountry = pulledCountry.property("value").trim();
-    let addedShape = pulledState.property("value").trim();
-    // create a new variable
-    //new variable will load the new filtered data to the page
-   
-    let filteredDate = tableData.filter(tableData => tableData.datetime === addedDate);
-    let filteredCity = tableData.filter(tableData => tableData.city === addedCity);
-    let filteredState = tableData.filter(tableData => tableData.state === addedState);
-    let filteredCountry = tableData.filter(tableData => tableData.country === addedCountry);
-    let filteredShape = tableData.filter(tableData => tableData.shape === addedShape);
-    
-    $tbody.html("");
+  // Filter on city
+  if (filterCity != "") {
+    tableData = tableData.filter(function (address) {
+      var addressCity = address.city;
+      return addressCity === filterCity;
+    });
+  }
+  else { tableData };
 
-    let response = {
-        filteredDate,
-        filteredCity,
-        filteredState,
-        filteredCountry,
-        filteredShape
-    }
-// add the filtered data to the page
-    if(response.filteredDate.length !== 0) {
-        newData(filteredDate);
-    }
-	elif(response.filteredCity.length !== 0) {
-        newData(filteredDate);
-    }
-    elif(response.filteredState.length !== 0) {
-        newData(filteredDate);
-    }
-    elif(response.filteredCountry.length !== 0) {
-        newData(filteredDate);
-    }
-    elif(response.filteredShape.length !== 0) {
-        newData(filteredDate);
-    }
-    // message will appear if the data you filtered does no appear
-        else {
-            $tbody.append("tr").append("td").text("No UFO Sightings");
-        }
-})
+  // Filter on country
+  if (filterCountry != "") {
+    tableData = tableData.filter(function (address) {
+      var addressCountry = address.country;
+      return addressCountry === filterCountry;
+    });
+  }
+  else { tableData };
 
+  // Filter on shape
+  if (filterShape != "") {
+    tableData = tableData.filter(function (address) {
+      var addressShape = address.shape;
+      return addressShape === filterShape;
+    });
+  }
+  else { tableData };
+
+  renderTable();
+}
+
+// Clear all the fields
+function handleResetButtonClick(){
+  renderTable();
+}
+
+// Render the table for the first time on page load
+renderTable();
